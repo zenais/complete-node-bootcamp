c05114b0f11b22c8f8ed3cdefda5b1198feb2ffb
zinaida <zinaida.j84@gmail.com>
2023-01-14 15:07:17 +0100

    using modules

--- 1-node-farm/starter_Z/index.js
+++ 1-node-farm/starter_Z/index.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const http = require("http");
 const url = require("url");
 
+const replaceTemplate = require("./modules/replaceTemplate");
+
 /* const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 console.log(textIn);
 
@@ -26,21 +28,6 @@ console.log("file has been written"); */
 console.log("Will read file");
 */
 ////////////////SERVER////////////////
-const replaceTemplate = (temp, product) => {
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%ID%}/g, product.id);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%DESCTIPTION%}/g, product.description);
-
-  if (!product.organic)
-    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
-
-  return output;
-};
 
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
