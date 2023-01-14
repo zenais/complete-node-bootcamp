a15ac72ccfb9782fa33113ebdfc3c75e09a9683b
zinaida <zinaida.j84@gmail.com>
2023-01-14 12:35:53 +0100

    filling templates

--- 1-node-farm/starter_Z/index.js
+++ 1-node-farm/starter_Z/index.js
@@ -26,6 +26,34 @@ console.log("file has been written"); */
 console.log("Will read file");
 */
 ////////////////SERVER////////////////
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%ID%}/g, product.id);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%DESCTIPTION%}/g, product.description);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+
+  return output;
+};
+
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
@@ -34,13 +62,26 @@ const server = http.createServer((req, res) => {
   console.log(req.url);
 
   const pathName = req.url;
+
+  //Overview Page
   if (pathName === "/" || pathName === "/overview") {
-    res.end("This is the OVERVIEW");
+    res.writeHead(200, { "Content.type": "text/html" });
+
+    const cardsHtml = dataObj.map((el) => replaceTemplate(tempCard, el)).join();
+    const output = tempOverview.replace(/{%PRODUCT_CARDS%}/g, cardsHtml);
+
+    res.end(output);
+
+    //API
   } else if (pathName === "/api") {
     res.writeHead(200, { "Content.type": "application/json" });
     res.end(data);
+
+    //Product page
   } else if (pathName === "/product") {
     res.end("This is the PRODUCT");
+
+    //Not found
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
