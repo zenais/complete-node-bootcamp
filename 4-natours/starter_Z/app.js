357695d5448800cd458df6c3b6dc2f0ed80d043a
zinaida <zinaida.j84@gmail.com>
2023-01-30 11:12:09 +0100

    start of section 6

--- 4-natours/starter_Z/app.js
+++ 4-natours/starter_Z/app.js
@@ -1,11 +1,18 @@
 const express = require('express');
-
 const app = express();
-
+/* 
 // root url
 app.get('/', (req, res) => {
-  res.status(200).send('Hello from the server side');
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side', app: 'natours' });
 });
+
+app.post('/', (req, res) => {
+  res.status(200);
+  res.send('You cam post on this URL');
+});
+ */
 
 const port = 3000;
 app.listen(port, () => {
