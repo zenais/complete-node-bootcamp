6e81ffcddc86f9d4e637e2993579a29ef07ca4b1
zinaida <zinaida.j84@gmail.com>
2023-01-30 12:14:41 +0100

    post request finished

--- 4-natours/starter_Z/app.js
+++ 4-natours/starter_Z/app.js
@@ -1,5 +1,9 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
 /* 
 // root url
 app.get('/', (req, res) => {
@@ -13,6 +17,39 @@ app.post('/', (req, res) => {
   res.send('You cam post on this URL');
 });
  */
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours.json`, 'utf8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours: tours,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours.json`,
+    JSON.stringify(tours),
+    () => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tours: newTour,
+        },
+      });
+    }
+  );
+});
 
 const port = 3000;
 app.listen(port, () => {
